e610b219377ddcd2b39f54db120f85508c955f4e
kangwsh <kangwsh22@163.com>
2022-03-11 11:08:07 +0800

    fix bug in 51.cpp

--- 51.cpp
+++ 51.cpp
@@ -8,19 +8,19 @@ class Solution{
 			vector<bool> column(n,true);
 			vector<bool> ldiag(2 * n - 1,true);
 			vector<bool> rdiag(2 * n - 1,true);
-			vector<string> (n,string(n,'.'));
+			vector<string> tmp(n,string(n,'.'));
 			vector<vector<string>> ans;
-			back_tracking(k,column,ldiag,rdiag,n,ans,tmp);
+			back_tracking(0,column,ldiag,rdiag,n,ans,tmp);
 			return ans;
 
 		}
-		void back_tracking(int k,vector<bool> &cloumn,vector<bool> &ldiag,vector<bool> &rdiag,int n,vector<vector<string>> &ans,vector<string> &tmp){
+		void back_tracking(int k,vector<bool> &column,vector<bool> &ldiag,vector<bool> &rdiag,int n,vector<vector<string>> &ans,vector<string> &tmp){
 			if(k == n){
 				ans.push_back(tmp);
 				return ;
 			}
 
-			for(int i = 0,i < n,i++){
+			for(int i = 0;i < n;i++){
 				if(column[i] && ldiag[i + k] && rdiag[n - 1 - k + i]){
 					tmp[k][i] = 'Q';
 					column[i] = false;
@@ -35,7 +35,6 @@ class Solution{
 			}
 		}
 };
-
 int main(){
 	return 0;
 }
