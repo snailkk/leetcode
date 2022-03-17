f351696b78f9dfd1fc547e741966428634c06549
kangwsh <kangwsh22@163.com>
2022-03-17 16:26:23 +0800

    路径总和

---
+++ 112路径总和.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+        if(!(root -> left) && !(root -> right)){
+            if(targetSum - root -> val == 0) return true;
+            else return false;
+        }
+        return hasPathSum(root -> left,targetSum - root -> val) || hasPathSum(root -> right,targetSum - root -> val);
+    }
+};
